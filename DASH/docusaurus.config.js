1b3ad96afb4de2b9e6f4d942df2e995bb1ef0370
Raul Viela <132754495+Ksk68@users.noreply.github.com>
2026-05-27 22:17:42 +0100

    feat: configuração para o site comparecer online no github

--- DASH/docusaurus.config.js
+++ DASH/docusaurus.config.js
@@ -12,22 +12,18 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'DASH',
   favicon: 'img/logo.ico',
+  url: 'https://github.com', // URL do Github
+  baseUrl: '/TP3-ADC/', // Nome do repositorio (entre /) ALTERAR
+  organizationName: 'Ksk68', // Nome do utilizador ALTERAR
+  projectName: 'TP3-ADC', // Nome do repositorio ALTERAR
+  deploymentBranch: 'gh-pages', // Nome do ramo a ser publicado
+  trailingSlash: false, // recomendado para o Github Pages
+
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
